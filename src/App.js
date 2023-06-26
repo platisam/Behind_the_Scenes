46ef6d6d1dc89d67fae86f14640312422c3404ca
platisam <platisam@gmail.com>
2023-06-26 17:35:24 +0200

    logging state during rendering

--- src/App.js
+++ src/App.js
@@ -26,7 +26,7 @@ export default function App() {
   );
 }
 
-console.log(<DifferentContent test={23} />);
+//console.log(<DifferentContent test={23} />);
 //console.log(DifferentContent());
 
 function Tabbed({ content }) {
@@ -68,6 +68,8 @@ function TabContent({ item }) {
   const [showDetails, setShowDetails] = useState(true);
   const [likes, setLikes] = useState(0);
 
+  console.log("RENDER");
+
   function handleInc() {
     setLikes(likes + 1);
   }
@@ -75,6 +77,7 @@ function TabContent({ item }) {
   function handleUndo() {
     setShowDetails(true);
     setLikes(0);
+    console.log(likes);
   }
 
   return (
