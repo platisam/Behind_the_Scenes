2ecdc0efeff13858bc8adb2413124fcfffe6075c
platisam <platisam@gmail.com>
2023-06-26 18:03:56 +0200

    setting handleUndoLater function for automatic batching

--- src/App.js
+++ src/App.js
@@ -89,6 +89,10 @@ function TabContent({ item }) {
     console.log(likes);
   }
 
+  function handleUndoLater() {
+    setTimeout(handleUndo, 2000);
+  }
+
   return (
     <div className="tab-content">
       <h4>{item.summary}</h4>
@@ -108,7 +112,7 @@ function TabContent({ item }) {
 
       <div className="tab-undo">
         <button onClick={handleUndo}>Undo</button>
-        <button>Undo in 2s</button>
+        <button onClick={handleUndoLater}>Undo in 2s</button>
       </div>
     </div>
   );
